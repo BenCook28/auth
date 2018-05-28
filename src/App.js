fdde8aa21b52c5eb1591ff92444e5a75af0a50e8
Benjamin Cook <benjamincook@benjamins-mbp.home>
2018-05-28 19:06:29 -0400

    gitignore test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: "AIzaSyCaz1MOOEfSv-XQldeabjgMydYO47pXdEA",
+            authDomain: "auth-3c5a3.firebaseapp.com",
+            databaseURL: "https://auth-3c5a3.firebaseio.com",
+            projectId: "auth-3c5a3",
+            storageBucket: "auth-3c5a3.appspot.com",
+            messagingSenderId: "992761254120"
+        });
+    }
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>Auth App</Text>
+                <LoginForm />
             </View>);
 
     }
